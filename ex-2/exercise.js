7e63ced5388e59148e1a2153c554e383edc54cc6
ahlam alshaba <ama1977ln@outlook.com>
2021-02-25 14:47:47 +0100

    added some pscode

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -7,3 +7,11 @@ var fuy = prompt("Please writ the future year that you want me to calculate", " 
 var newAge = fuy - dob;
 
 window.alert("You will be either " +newAge+ " or " +(newAge-1)+ " in " +fuy+ ".")
+
+
+// extra
+// it is the same just we need to add 2 prompt to ask for the day of birth and to ask for month of birth
+// and add 2 var as var newday = dayOfFuture - dayOfBerthday, and the same for the month.
+
+
+
