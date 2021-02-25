daf1c3628f247714034856e9665a90a1f79bec5e
ahlam alshaba <ama1977ln@outlook.com>
2021-02-25 15:37:52 +0100

    solved

--- ex-3/README.md
+++ ex-3/README.md
@@ -9,3 +9,4 @@ Log the output, e.g. "The greater number of 5 and 10 is 10."
 Log the output, e.g. "The greater number of 5 and 10 is 5."
 
 Add an output for the else statement, e.g. "The numbers are the same."
+
--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,18 @@
 // Example
-window.alert("Hello World!");
+var x = prompt("Wirte the first number");
+var y = prompt("Wirte the second number");
+
+if (x>y) {
+    
+    window.alert( " The greater number of " + x + " than" + y);
+}
+
+else if (x<y) {
+    
+    window.alert(" The greater number of " + y + " than" + x );
+}
+
+else {
+    
+    window.alert("The numbers are the same.");
+}
