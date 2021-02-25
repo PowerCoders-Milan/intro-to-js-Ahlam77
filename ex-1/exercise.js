f9f98163767b804edac226b3cac43c7113810f06
ahlam alshaba <ama1977ln@outlook.com>
2021-02-25 12:14:07 +0100

    finished ex1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,9 @@
 // Example
 window.alert("Hello World!");
+
+var numberOfChildren = 3;
+var father = " Ali ";
+var mother = " Mariam";
+var geographicLocation = "Us";
+var jobTitel = "Doctor";
+console.log ( "You will be a " + jobTitel + " in " + geographicLocation + " You will be married to "+ father +" And will you have " + numberOfChildren )
