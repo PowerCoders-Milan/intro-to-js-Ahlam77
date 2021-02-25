721438f6bef03adb287e8ed02c2171e6aff43dae
ahlam alshaba <ama1977ln@outlook.com>
2021-02-25 14:31:12 +0100

    finshied i think

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,4 +1,9 @@
 // Example
-window.alert("Hello World!");
 
-var bornYear = prompt ("Pleas insert ")
+
+var dob = prompt("Please writ your year of birthday ", " ");  
+var fuy = prompt("Please writ the future year that you want me to calculate", " ")
+
+var newAge = fuy - dob;
+
+window.alert("You will be either " +newAge+ " or " +(newAge-1)+ " in " +fuy+ ".")
